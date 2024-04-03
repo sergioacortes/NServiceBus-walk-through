273ba8609f7d89212a8c1d26b71b720da62c9f84
Sergio Antonio Cortes Ocampo <sergio.cortes>
2024-04-03 10:01:33 +0200

    Create and configure projects

---
+++ src/NServiceBusSample.Contracts/CreateBookCommand.cs
@@ -0,0 +1,12 @@
+﻿namespace NServiceBusSample.Contracts;
+
+public class CreateBookCommand
+{
+    
+    public Guid BookId { get; set; }
+
+    public string Title { get; set; }
+
+    public string Author { get; set; }
+    
+}
