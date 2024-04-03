734f388806b3bca37bfb684185948bf466bd27cf
Sergio Antonio Cortes Ocampo <sergio.cortes>
2024-04-03 10:01:33 +0200

    Create and configure projects

---
+++ src/NServiceBusSample.Consumer/Program.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.Extensions.Hosting;
+
+var builder = Host.CreateDefaultBuilder()
+    .ConfigureAppConfiguration((context, configurationBuilder) => { })
+    .ConfigureServices((builderContext, services) => { });
+
+var host = builder.Build();
+
+await host.StartAsync();
