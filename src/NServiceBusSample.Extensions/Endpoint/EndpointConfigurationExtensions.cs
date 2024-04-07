657dbad580c9469717a001a70cc9121088d06ba8
Sergio Antonio Cortes Ocampo <Sergio.cortes@wolterskluwer.com>
2024-04-07 23:28:15 +0200

    Add NServiceBus extensions

---
+++ src/NServiceBusSample.Extensions/Endpoint/EndpointConfigurationExtensions.cs
@@ -0,0 +1,33 @@
+﻿namespace NServiceBusSample.Extensions.Endpoint;
+
+public static class EndpointConfigurationExtensions
+{
+    
+    public static EndpointConfiguration DefineAsCommand(this EndpointConfiguration endpointConfiguration, params Type[] types)
+    {
+        endpointConfiguration.Conventions().DefiningCommandsAs(EndpointConfigurationExtensions.TypesDefinitionsAreValid(types));
+        return endpointConfiguration;
+    }
+
+    public static EndpointConfiguration DefineAsEvent(this EndpointConfiguration endpointConfiguration, params Type[] types)
+    {
+        endpointConfiguration.Conventions().DefiningEventsAs(EndpointConfigurationExtensions.TypesDefinitionsAreValid(types));
+        return endpointConfiguration;
+    }
+
+    public static EndpointConfiguration DefineAsMessage(this EndpointConfiguration endpointConfiguration, params Type[] types)
+    {
+        endpointConfiguration.Conventions().DefiningMessagesAs(EndpointConfigurationExtensions.TypesDefinitionsAreValid(types));
+        return endpointConfiguration;
+    }
+    
+    private static Func<Type, bool> TypesDefinitionsAreValid(Type[] types)
+    {
+        return (Func<Type, bool>) (type => 
+            type.Namespace != null && 
+            (type.IsInterface && !type.IsGenericType || type.IsClass && !type.IsAbstract) && 
+            ((IEnumerable<Type>) types).Any<Type>((Func<Type, bool>) (t => t.IsAssignableFrom(type)))
+        );
+    }
+    
+}
---
+++ src/NServiceBusSample.Extensions/Routing/RouteEndpointType.cs
@@ -0,0 +1,10 @@
+﻿namespace NServiceBusSample.Extensions.Routing;
+
+public class RouteEndpointType
+{
+    
+    public string Type { get; set; }
+
+    public string Endpoint { get; set; }
+    
+}
